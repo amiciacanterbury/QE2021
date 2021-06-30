a9d3dfbbb3ed5ccc828fbc545afd77bcbaab9667
Amicia <3567213@myuwc.ac.za>
2021-06-30 21:53:10 +0200

    Gamma_diversity_questions_only

---
+++ Amicia_Canterbury_Gamma_Diversity.R
@@ -0,0 +1,21 @@
+# Amicia Canterbury
+# 30 June 2021
+# Quantitative Ecology
+# Gamma-diversity questions
+
+# I did the gamma-diversity code with my Day 1: Measures of biodiversity.
+# I pushed it on Tuesday 29 June already. I am just adding it below with 
+# the questions I missed. 
+
+#-----------------------------Gamma diversity-----------------------------------
+
+# 1.ncol() counts the total number of columns. pooled() calculates the sp. richness
+# across all the columns. It is different because there is a species that does 
+# not occur at any of the sites. 
+# Species richness counts the number of total species found in the sites being
+# looked at. In gamma-diversity, when the data is pooled(), the pooled() takes out
+# rare species that may cause "noise". 
+
+# 2. pooled() data is right due to the fact that the species that does not occur
+# within any of the sites, causing extra noise. Excluding it would calculate the
+# actual gamma diversity of the area being looked at. 
